cc945637889a209ee0f7599da0b8a2206cf8f901
Édouard Lopez <dev+github@edouard-lopez.com>
2013-03-31 23:05:36 +0200

    Add function to loop all shell history

--- shell-stats.bash
+++ shell-stats.bash
@@ -5,3 +5,13 @@
 #
 
 shellList = ( "bash" "csh" "ksh" "sh" "zsh" )
+
+
+# @description For all shell, we look at the history file
+#
+function loopShells() {
+    for s in "$shellList[$*]";
+    do
+        printf "%s\n" "$s"
+    done
+}
