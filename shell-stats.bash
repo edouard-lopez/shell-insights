79a8a64c2dd906a7efb927d9d4882d3e26802ae2
Édouard Lopez <dev+github@edouard-lopez.com>
2013-03-31 23:50:28 +0200

    Add crawler function for each shell

--- shell-stats.bash
+++ shell-stats.bash
@@ -4,14 +4,62 @@
 # USAGE
 #
 
-shellList = ( "bash" "csh" "ksh" "sh" "zsh" )
+scriptDir="$(dirname "$0")" # emplacement du script
 
+shellList=( "bash" "csh" "ksh" "sh" "tsh" "zsh" )
 
 # @description For all shell, we look at the history file
 #
 function loopShells() {
-    for s in "$shellList[$*]";
+    for s in "${shellList[@]}";
     do
-        printf "%s\n" "$s"
+        crawlHistory "$s"
     done
 }
+
+
+function crawlHistory() {
+    shell="$1"
+
+    printf -v historyFile "%s/.%s_history" "$HOME" "$shell"
+    "$shell"Crawler
+}
+
+# @description  crawl Bash history
+function bashCrawler() {
+    printf "%s\n" "Bash"
+  awk -f "$scriptDir"/crawler-bash.awk "$historyFile"
+}
+
+# @description  crawl Csh history
+function cshCrawler() {
+    printf "%s\n" "Csh"
+  awk -f "$scriptDir"/crawler-csh.awk "$historyFile"
+}
+
+# @description  crawl Ksh history
+function kshCrawler() {
+    printf "%s\n" "Ksh"
+  awk -f "$scriptDir"/crawler-ksh.awk "$historyFile"
+}
+
+# @description  crawl Sh history
+function shCrawler() {
+    printf "%s\n" "Sh"
+  awk -f "$scriptDir"/crawler-sh.awk "$historyFile"
+}
+
+# @description  crawl Tsh history
+function tshCrawler() {
+    printf "%s\n" "Tsh"
+  awk -f "$scriptDir"/crawler-tsh.awk "$historyFile"
+}
+
+# @description  crawl Zsh history
+function zshCrawler() {
+    printf "%s\n" "Zsh"
+    cut -d ';' -f 2- "$historyFile" | awk -f "$scriptDir"/crawler-zsh.awk
+}
+
+
+loopShells "${shellList[@]}"
