f15e07f257bf9383a7ebc2e22b5c6c0767c01b40
Édouard Lopez <dev+github@edouard-lopez.com>
2013-04-02 18:56:35 +0200

    update indentation
    Signed-off-by: Édouard Lopez <dev+github@edouard-lopez.com>

--- shell-stats.bash
+++ shell-stats.bash
@@ -4,56 +4,54 @@
 # USAGE
 #
 
-
 scriptDir="$(dirname "$0")" # emplacement du script
 
 shellList=( "bash" "csh" "ksh" "sh" "tsh" "zsh" )
 
 # @description For all shell, we look at the history file
-#
 function loopShells() {
-    for shell in "${shellList[@]}";
-    do
-      printf -v historyFile "%s/.%s_history" "$HOME" "$shell"
+  for shell in "${shellList[@]}";
+  do
+    printf -v historyFile "%s/.%s_history" "$HOME" "$shell"
 
-      if [[ -e "$historyFile" || -f "$historyFile" ]];
+    if [[ -e "$historyFile" || -f "$historyFile" ]];
       then
         $(sed -i '/shell-stats.bash/d' "$historyFile") # remove references to the script
         "$shell"Crawler # call the right crawler
-      else
+    else
         printf "skipping %s\n" "$shell"
-      fi
-    done
+    fi
+done
 }
 
 
 # @description  crawl Bash history
 function bashCrawler() {
-    printf "%s\n" "Bash"
+  printf "%s\n" "Bash"
   awk -f "$scriptDir"/crawler-bash.awk "$historyFile"
 }
 
 # @description  crawl Csh history
 function cshCrawler() {
-    printf "%s\n" "Csh"
+  printf "%s\n" "Csh"
   awk -f "$scriptDir"/crawler-csh.awk "$historyFile"
 }
 
 # @description  crawl Ksh history
 function kshCrawler() {
-    printf "%s\n" "Ksh"
+  printf "%s\n" "Ksh"
   awk -f "$scriptDir"/crawler-ksh.awk "$historyFile"
 }
 
 # @description  crawl Sh history
 function shCrawler() {
-    printf "%s\n" "Sh"
+  printf "%s\n" "Sh"
   awk -f "$scriptDir"/crawler-sh.awk "$historyFile"
 }
 
 # @description  crawl Tsh history
 function tshCrawler() {
-    printf "%s\n" "Tsh"
+  printf "%s\n" "Tsh"
   awk -f "$scriptDir"/crawler-tsh.awk "$historyFile"
 }
 
