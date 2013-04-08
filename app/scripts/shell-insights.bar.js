b89143e1b435aa9903703607e329dbc833c18a54
Édouard Lopez <dev+github@edouard-lopez.com>
2013-04-08 17:25:40 +0200

    Declare canvas dimension

--- app/scripts/shell-insights.bar.js
+++ app/scripts/shell-insights.bar.js
@@ -3,6 +3,11 @@ var d3, $; // sublime-text-2
 function draw(data) {
     'use strict';
 
+    // canvas properties
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = data.length*10 - (margin.left + margin.right),
+    height = 500 - (margin.top + margin.bottom);
+
 }
 
 
