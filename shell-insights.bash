899529ab958ce69d21b677bf7e7aec7b44bf8fc1
Édouard Lopez <dev+github@edouard-lopez.com>
2013-04-08 16:25:01 +0200

    remove index as we use an array to hold all our objects
    Signed-off-by: Édouard Lopez <dev+github@edouard-lopez.com>

--- shell-insights.bash
+++ shell-insights.bash
@@ -104,9 +104,9 @@ function toJSON() {
     # so we get byte-for-byte content
     while IFS='' read -r -d '' cmdKey && IFS='' read -r -d '' count;
     do
-        printf "\"%s\": %s, \n" "$i" "$object"
         cmd=( -s "$cmdKey" -i "cmd"  -n "$count" -i "size" )
         object="$("$scriptDir"/jshon/jshon "${cmd[@]}"<<<$cmdWrap )"
+        printf "%s" "$object" # create [{ object }, ...]
         let "i++"
     done
     printf "\n%s" "${globalWrap[1]}"
